95bc5bb8ccf12a1f664220c246f5abd722b6ac0d
Dmitry-15 <dimaggio.dima@mail.ru>
2021-11-15 22:22:13 +0300

    Задания

--- Zadaniy/primer2.py
+++ Zadaniy/primer2.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
-tpl = lambda a, b: (a, b)
-a = tpl(1, 2)
-print(a)
-b = tpl(3, a)
-print(b)
-c = tpl(a, b)
-print(c)
+if __name__ == '__main__':
+    tpl = lambda a, b: (a, b)
+    a = tpl(1, 2)
+    b = tpl(3, a)
+    c = tpl(a, b)
+    print(f"a = {a}\nb = {b}\nc = {c}")
